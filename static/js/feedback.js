60ce0e529c390bc21df6b9d89eddd8cb79ab1ac2
magabrielaa <90418921+magabrielaa@users.noreply.github.com>
2022-12-02 11:15:31 -0600

    map fill by selection working

--- static/js/feedback.js
+++ static/js/feedback.js
@@ -1,46 +1,178 @@
 document.addEventListener("DOMContentLoaded", setupListeners)
 
 function setupListeners () {
-    const checkbox = document.querySelector("#checkbox")
-    checkbox.addEventListener('change', handleCheckbox)
+    const dropdown1 = document.querySelector("#dropdown-1")
+    dropdown1.addEventListener('change', handleDropdown1)
+    const dropdown2 = document.querySelector("#dropdown-2")
+    dropdown2.addEventListener('change', handleDropdown2)
 }
 
-function handleCheckbox (e){
-    if (e.target.checked) {
-        console.log("Checkbox is checked.")
-        wardNumber = document.querySelector('.t:checked').value
-        console.log(wardNumber)
-        return wardNumber;
-      } else {
-        console.log("Checkbox is not checked.");
-      }
+//initialize ward numbers
+window.wardNumber1 = "w29"
+window.wardNumber2 = "w35"
+
+
+function handleDropdown1 (e){
+    let wardNumber = e.target.value.slice(5,7)
+    window.wardNumber1 = "w" + wardNumber
+    showWard1Data(wardNumber)
+    updateMap()
 }
 
-// callbacks to record and manage responses
-// also, any part of the DOM that is data-driven
-// need to wait until page has loaded all html elements
+function handleDropdown2 (e){
+    let wardNumber = e.target.value.slice(5,7)
+    window.wardNumber2 = "w" + wardNumber
+    console.log(window.wardNumber2)
+    showWard2Data(wardNumber)
+    updateMap()
+}
 
-d3.csv("../../data/race.csv").then(data => { 
-    console.log("view data", data)
-    let chartFxn = barChart(); 
-    chartFxn.xVar("race");
-    chartFxn.yVar("ward_29");
-    d3.select('#ward2')
-        .datum(data)
-        .call(chartFxn);
+function showWard1Data(wardNumber) {
+    d3.json("../../data/wards_data.json").then(data => { 
+        let yMax = 0
+        for(var ward in data) {
+                df = data[ward]
+                df.forEach((e) => {
+                    if (e.count > yMax) {
+                        yMax = e.count
+                    }
+                }
+                )
+            }
+
+        for(var ward in data) {
+
+                if (ward.slice(5,7) == wardNumber){
+    
+                    isRaceChartEmpty = document.querySelectorAll(".race-chart-1")
+                    isIncomeChartEmpty = document.querySelectorAll(".income-chart-1")
+  
+                    if (isRaceChartEmpty.length != 0) {
+                        isRaceChartEmpty.forEach(function(i) {
+                            i.remove()
+                          })
+                    }
+                
+                    if (isIncomeChartEmpty.length != 0) {
+                        isIncomeChartEmpty.forEach(function(i) {
+                            i.remove()
+                          })
+                    }
+                    
+                    filteredData = data[ward]
+                
+                    let raceChart = barChart(); 
+                    raceChart.xVar("race");
+                    raceChart.yVar("count");
+                    raceChart.barClass("bar-chart-1");
+                    raceChart.svgClass("race-chart-1");
+                    raceChart.yMax(yMax);
+
+                    d3.select("#w1c1")
+                        .datum(filteredData)
+                        .call(raceChart);
+                      
+                    let incomeChart = barChart(); 
+                    incomeChart.xVar("income");
+                    incomeChart.yVar("count");
+                    incomeChart.barClass("bar-chart-1");
+                    incomeChart.svgClass("income-chart-1");
+                    incomeChart.yMax(yMax);
+                    d3.select("#w1c2")
+                        .datum(filteredData)
+                        .call(incomeChart);
+                    }
+            }
+         }
+    )      
+    }
+
+function showWard2Data(wardNumber) {
+    d3.json("../../data/wards_data.json").then(data => { 
+
+        let yMax = 0
+        for(var ward in data) {
+                df = data[ward]
+                df.forEach((e) => {
+                    if (e.count > yMax) {
+                        yMax = e.count
+                    }
+                }
+                )
+            }
+
+        for(var ward in data) {
+                if (ward.slice(5,7) == wardNumber){
+                    isRaceChartEmpty = document.getElementsByClassName("race-chart-2")
+                    isIncomeChartEmpty = document.querySelectorAll(".income-chart-2")
+
+                    if (isRaceChartEmpty.length != 0) {
+                        isRaceChartEmpty.forEach(function(i) {
+                            i.remove()
+                          })
+                    }
+            
+                    if (isIncomeChartEmpty.length != 0) {
+                        isIncomeChartEmpty.forEach(function(i) {
+                            i.remove()
+                          })
+                    }
+
+                    filteredData = data[ward]
+
+                    let raceChart = barChart(); 
+                    raceChart.xVar("race");
+                    raceChart.yVar("count");
+                    raceChart.barClass("bar-chart-2");
+                    raceChart.svgClass("race-chart-2");
+                    raceChart.yMax(yMax);
+                    d3.select("#w2c1")
+                        .datum(filteredData)
+                        .call(raceChart);
+
+                    let incomeChart = barChart(); 
+                    incomeChart.xVar("income");
+                    incomeChart.yVar("count");
+                    incomeChart.barClass("bar-chart-2");
+                    incomeChart.svgClass("income-chart-2");
+                    incomeChart.yMax(yMax);
+                    d3.select("#w2c2")
+                        .datum(filteredData)
+                        .call(incomeChart);
+                    }
+            }
+         }
+    )      
+    }
+
+
+// make map chart instance a global variable
+let chiMap = map(); // this is a promise
+
+// wrap the required stuff in Promises
+const mapRef = new Promise(function (resolve, reject) {
+    resolve(chiMap);
+});
+const data = d3.json("../../data/ward_boundaries.geojson");
+
+// When both of these Promises have resolved, call the chart component, etc.
+Promise.all([mapRef, data])
+    .then(function (values) {
+        let [chiMap, data] = values; //object destructuring
+
+        chiMap.wardSelectionOne(window.wardNumber1)
+        chiMap.wardSelectionTwo(window.wardNumber2)
+
+        d3.select("#map-container")
+            .datum(data)
+            .call(chiMap);
+    })
+    .catch(function (err) {
+        console.log("error", err);
     });
 
-
-// $(document).ready(function () { //jquery
-
-//     // mount chart components
-//     d3.csv("../../data/race.csv").then(data => { 
-//         let chartFxn = barChart(data);
-//         chartFxn.xVar("race")
-//         chartFxn.yVar("ward_29")
-//         d3.select('#ward2')
-//             .datum(data)
-//             .call(chartFxn);
-//     })
-// });
-
+function updateMap(){
+        chiMap.wardSelectionOne(window.wardNumber1)
+        chiMap.wardSelectionTwo(window.wardNumber2)
+        chiMap.render();
+}
